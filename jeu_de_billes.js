5ac4b95bcdff730dad426450b10e05a48cf76879
Fidomi <sidonie.moulart@gmail.com>
2020-12-22 09:42:42 +0100

    Initial commit of Jeu de Billes

---
+++ jeu_de_billes.js
@@ -0,0 +1,37 @@
+/*  NOTIONS A UTILISER DANS LE PROJET
+
+// Toggle entre fond rouge et pas de fond
+    // Donne aléatoirement une valeur du tableau
+        var tab = [10, 20, 30, 50];
+        let k = document.getElementById("keyc");
+        const ESPACE = 32;
+        document.addEventListener("keyup", function(e){
+            console.log(e.keyCode);
+            if (e.keyCode == ESPACE){
+                k.classList.toggle('d-none');
+                console.log(tab[Math.floor(Math.random()*tab.length)]);
+            }
+        });
+
+    //POUR EXECUTER QQCH AU CHARGEMENT DU DOCUMENT
+        document.addEventListener('DOMContentLoaded', function(e){
+            console.log(e.target);
+        })
+
+        addEventListener("click",function(e){
+                console.log(e.target);});
+
+
+        //POUR DESACTIVER DES BOUTONS
+        document.getElementById('ajouter').addEventListener("blur", function(){console.log("blur")});
+        document.getElementById('effacer').disabled=false;
+        document.getElementById('effacer').addEventListener("keyup", function(){ocument.getElementById('ajouter').disabled=false;});
+        let btn = document.getElementsByClassName('bouton');
+        for (let i=0; i<btn; i++){
+            btn[i].addEventListener("click",function(e){
+                console.log(e.target);
+            })
+
+        }
+
+*/
