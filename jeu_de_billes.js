0c545fbab4511ae3c3b359f42a8e1d2ec99e726c
Fidomi <sidonie.moulart@gmail.com>
2020-12-30 21:49:18 +0100

    Debut de la partie a 2 joueurs

--- jeu_de_billes.js
+++ jeu_de_billes.js
@@ -44,17 +44,26 @@ var messageJ = document.getElementById("messageJoueur");
 var messageB = document.getElementById("messageBilles");
 var tabJeu = [];
 
-//Variables pour l'enregistrement des joueurs
+//Variables pour le choix du nombre de joueurs
 var unJoueur = document.getElementById("un-joueur");
 var deuxJoueurs = document.getElementById("deux-joueurs");
 var circle = document.getElementById("circle");
 
-//Variables pour la partie de jeu
+//Variables pour l'enregistrement des joueurs
 var affichage_billes = document.getElementById("billes");
 var joueurUnOK = document.getElementById("un-joueur-ok");
 var joueurDeuxOK = document.getElementById("deux-joueurs-ok");
 const activeJoueur1 = document.getElementById("un-joueur");
 const activeJoueur2 = document.getElementById("deux-joueurs");
+
+//Variables pour l'enregistrement des joueurs
+var nomJoueurUn = document.getElementById("nomJoueurUn");
+var nomJoueurDeux = document.getElementById("nomJoueurDeux");
+var avatarJoueurUn  = document.getElementById("avatarJoueurUn");
+var avatarJoueurDeux  = document.getElementById("avatarJoueurDeux");
+
+
+
 /*FONCTIONS OUTILS*/
 
 function toggle(target, cclass) {
@@ -87,7 +96,7 @@ letsPlay.addEventListener("click", function (event) {
         on affiche la page d'inscription*/
   if (tabJeu[0] && tabJeu[2] && tabJeu[2]) {
     var tabActiveJoueur1 = affichageInscription2();
-    
+
     //On enregistre les infos des joueurs
     joueurUnOK.addEventListener("click", function (e) {
       e.preventDefault();
@@ -97,7 +106,10 @@ letsPlay.addEventListener("click", function (event) {
     joueurDeuxOK.addEventListener("click", function (e) {
       e.preventDefault();
       validerInscriptionJoueur2();
+      partie2Joueurs();
     });
+
+    
 
     //Fin affichage du jeu
   }
@@ -138,7 +150,7 @@ function affichageInscription2() {
   toggle("regles", "hide");
   toggle("menu-jeu-02", "hide");
   toggle("menu-jeu", "hide");
-  toggle("enregistrement","hide");
+  toggle("enregistrement", "hide");
 
   if (tabJeu[1] == 0) {
     toggle("un-joueur", "hide");
@@ -153,10 +165,11 @@ function affichageInscription2() {
   affichage_billes.innerText = tabJeu[2];
   //Activer inscription joueur 1
   var tabActiveJoueur1 = activeJoueur1.querySelectorAll("[disabled]");
-  for (i = 0; i < tabActiveJoueur1.length; i++) {
+  for (let i = 0; i < tabActiveJoueur1.length; i++) {
     tabActiveJoueur1[i].disabled = false;
   }
-  return tabActiveJoueur1;
+    return tabActiveJoueur1;
+
 }
 
 /*********Fonction pour valider l'inscription du joueur 1***********/
@@ -168,15 +181,15 @@ function validerInscriptionJoueur1(tabActiveJoueur1) {
   var name1 = document.getElementById("name1");
   var listeAvatar1 = document.querySelectorAll('input[name="avatar1"]');
   var avatarExiste = false;
-  for (let i=0; i<listeAvatar1.length; i++) {
+  for (let i = 0; i < listeAvatar1.length; i++) {
     if (listeAvatar1[i].checked) {
       var avatarExiste = true;
       var avatar1 = listeAvatar1[i].value;
     }
   }
   if (name1.value && avatarExiste) {
-    tabJeu[0] = { name: name1.value, avatar: avatar1};
-    for (i = 0; i < tabActiveJoueur1.length; i++) {
+    tabJeu[0] = { name: name1.value, avatar: avatar1 };
+    for (let i= 0; i < tabActiveJoueur1.length; i++) {
       tabActiveJoueur1[i].disabled = true;
       tabActiveJoueur2[i].disabled = false;
     }
@@ -190,7 +203,7 @@ function validerInscriptionJoueur2() {
   var name2 = document.getElementById("name2").value;
   var listeAvatar2 = document.querySelectorAll('input[name="avatar2"]');
   var avatarExiste = false;
-  for (let i=0; i<listeAvatar2.length; i++) {
+  for (let i = 0; i < listeAvatar2.length; i++) {
     if (listeAvatar2[i].checked) {
       var avatarExiste = true;
       var avatar2 = listeAvatar2[i].value;
@@ -198,5 +211,65 @@ function validerInscriptionJoueur2() {
   }
   if (name2 && avatarExiste) {
     tabJeu[1] = { name: name2, avatar: avatar2 };
+    toggle("enregistrement", "hide");
   }
+}
+
+/*********Fonction pour lancer la partie de jeu à 2 joueurs***********/
+/*********Fonction pour lancer la partie de jeu à 2 jouers***********/
+function partie2Joueurs() {
+  //Affiche la partie de jeu
+  toggle("partie2joueurs", "hide");
+  toggle("lancement_jeu","hide");
+ //Entre les données des deux joueurs
+  nomJoueurUn.innerText = tabJeu[0].name;
+  nomJoueurDeux.innerText = tabJeu[1].name;
+  avatarJoueurUn.src = 'images/' + tabJeu[0].avatar +'.png';
+  avatarJoueurDeux.src = 'images/' + tabJeu[1].avatar +'.png';
+
+ // On remplit le tableau de billes
+ //On va cherche le tableau myTable dans la page html
+ var table = document.getElementById("myTable");
+ //Déterminer le nombre de lignes à 10 billes et le reste
+ var nbBilles = parseInt(tabJeu[2]);
+ var nbRows = Math.floor(nbBilles/10);
+ var nbBillesRestantes = nbBilles%10;
+ var tabBilles=[];
+ console.log(tabBilles);
+ console.log(nbBilles);
+
+ //On crée un tableau avec toutes les billes:
+ for (let i=0; i<nbBilles; i++){
+   tabBilles.push(i);
+ }
+
+ console.log(tabBilles);
+
+// On crée un array avec des lignes à 10 billes
+//  var tabBilles=[];
+//  for (let i=0, i<nbBilles, i++){
+//   tabBilles.push(i);
+//  }
+//  console.log(tabBilles);
+
+
+  // var row = table.insertRow(i);
+  //   for (let j=0, j<10, j++){
+  //     var cell[j]=row.insertCell(j);
+  //   }
+ 
+//  var row = table.insertRow(0);
+//  //S'il reste des billes on les met sur une dernière ligne
+
+// // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+// var cell1 = row.insertCell(0);
+// var cell2 = row.insertCell(1);
+
+// // Add some text to the new cells:
+// cell1.innerHTML = "NEW CELL1";
+// cell2.innerHTML = "NEW CELL2";
+
+
+
+  
 }
